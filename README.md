22b854a5c6a71dcc1f29030064b95f3f7596f5a3
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-07-24 08:11:03 +0330

    Add charts

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A reactJS admin panel for my online shop.
+
+Since there is no back-end for this project yet I'll fill it with dummy data and no requests.
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,66 @@
 import { Col, Divider, Row } from 'antd';
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 import { Stats } from '../../components';
 import formatter from '../../components/countup';
+
+const dailySaleData = [
+  { date: '1', revenue: 500 },
+  { date: '2', revenue: 600 },
+  { date: '3', revenue: 400 },
+  { date: '4', revenue: 750 },
+  { date: '5', revenue: 1000 },
+  { date: '6', revenue: 900 },
+  { date: '7', revenue: 800 },
+  { date: '8', revenue: 650 },
+  { date: '9', revenue: 850 },
+  { date: '10', revenue: 700 },
+  { date: '11', revenue: 950 },
+  { date: '12', revenue: 1100 },
+  { date: '13', revenue: 800 },
+  { date: '14', revenue: 750 },
+  { date: '15', revenue: 600 },
+  { date: '16', revenue: 400 },
+  { date: '17', revenue: 900 },
+  { date: '18', revenue: 750 },
+  { date: '19', revenue: 1000 },
+  { date: '20', revenue: 800 },
+  { date: '21', revenue: 700 },
+  { date: '22', revenue: 850 },
+  { date: '23', revenue: 750 },
+  { date: '24', revenue: 1100 },
+  { date: '25', revenue: 900 },
+  { date: '26', revenue: 750 },
+  { date: '27', revenue: 600 },
+  { date: '28', revenue: 950 },
+  { date: '29', revenue: 800 },
+  { date: '30', revenue: 700 },
+];
+
+const monthlySaleData = [
+  { month: '1', revenue: 500 },
+  { month: '2', revenue: 600 },
+  { month: '3', revenue: 400 },
+  { month: '4', revenue: 750 },
+  { month: '5', revenue: 1000 },
+  { month: '6', revenue: 900 },
+  { month: '7', revenue: 800 },
+  { month: '8', revenue: 650 },
+  { month: '9', revenue: 850 },
+  { month: '10', revenue: 700 },
+  { month: '11', revenue: 950 },
+  { month: '12', revenue: 1100 },
+];
 
 const Dashboard = () => {
   return (
@@ -130,6 +189,35 @@ const Dashboard = () => {
         s2v={654}
         bgGradient={['#b496d6', '#8597da']}
       />
+      <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+        <h1 style={{ fontWeight: 400, marginBottom: 24 }}>Daily Sale</h1>
+        <ResponsiveContainer width="100%" height="100%" maxHeight={400}>
+          <LineChart width={100} height={300} data={dailySaleData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis dataKey="revenue" />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="revenue"
+              stroke="#6b8068"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </Col>
+      <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+        <h1 style={{ fontWeight: 400, marginBottom: 24 }}>Monthly Sale</h1>
+        <ResponsiveContainer width="100%" height="100%" maxHeight={400}>
+          <BarChart width={150} height={40} data={monthlySaleData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis dataKey="revenue" />
+            <Tooltip />
+            <Bar dataKey="revenue" fill="#6b8068" />
+          </BarChart>
+        </ResponsiveContainer>
+      </Col>
     </Row>
   );
 };
