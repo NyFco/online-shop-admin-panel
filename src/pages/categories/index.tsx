2158d278e5fa34c6cbf4187378fc34ca1165d885
Yazdan Vahidi Sharbaaf <yazdanvahidi@yahoo.com>
2023-08-17 07:33:44 +0330

    fix #12

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { EditOutlined } from '@ant-design/icons';
 import { Button, Table } from 'antd';
 
+import AddCategoryModal from './AddCategoryModal';
 import EditCategoryModal from './EditCategoryModal';
 
 interface DataType {
@@ -30,15 +31,39 @@ const data: DataType[] = [
 
 const CategoriesPage = () => {
   const [editModalIsVisible, setEditModalIsVisible] = useState<boolean>(false);
+  const [addModalIsVisible, setAddModalIsVisible] = useState<boolean>(false);
   const [editItemIdx, setEditItemIdx] = useState<number>(0);
 
   return (
     <>
+      <AddCategoryModal
+        open={addModalIsVisible}
+        setOpen={setAddModalIsVisible}
+      />
       <EditCategoryModal
         open={editModalIsVisible}
         setOpen={setEditModalIsVisible}
         categoryData={data[editItemIdx]}
       />
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginBottom: 24,
+        }}
+      >
+        <Button
+          size="large"
+          style={{ margin: 0 }}
+          type="primary"
+          onClick={() => {
+            setAddModalIsVisible(true);
+          }}
+        >
+          Add New Category
+        </Button>
+      </div>
       <Table dataSource={data}>
         <Table.Column
           title="ID"
